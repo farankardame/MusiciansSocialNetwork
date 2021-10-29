986254ba2ce61413d6a28d9aca5979a27cb36afc
fkardame <faran.kardame@first-utility.com>
2021-10-29 21:23:44 +0100

    Initial Commit - Spring Boot

---
+++ src/main/java/com/social/network/musicians/MusiciansSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.social.network.musicians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiciansSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiciansSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/network/musicians/MusiciansSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.network.musicians;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiciansSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
